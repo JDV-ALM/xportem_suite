11b7e3513622cd676c5c75b3db859357b7c6f4b8
Jesus V <jveliz@almus.dev>
2025-06-20 16:19:08 -0400

    sample

--- xportem_project_extension/__manifest__.py
+++ xportem_project_extension/__manifest__.py
@@ -4,8 +4,8 @@
     'version': '18.0.1.0.1',
     'depends': ['base', 'mail', 'xportem_base', 'xportem_project'],
     'data': [
-        # Test 1: Only security
         'security/procurement_security.xml',
         'security/ir.model.access.csv',
-    ],
+        'data/sample_sequence_data.xml',
+],
 }
